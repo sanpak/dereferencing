24fbeaaad15784c743e5d71bfd9bf0e3b36ec844
Sanpak Ng <>
2021-04-16 01:53:17 +0000

    temperature

--- main.cpp
+++ main.cpp
@@ -13,4 +13,13 @@ int main() {
 
   cout << *score_ptr << endl;
   cout << score << endl;
+
+  cout << "\n-------------------------" << endl;
+  double high_temp {100.7};
+  double low_temp {37.4};
+  double *temp_ptr {&high_temp};
+
+  cout << *temp_ptr << endl;
+  temp_ptr = &low_temp;
+  cout << *temp_ptr << endl;
 }
