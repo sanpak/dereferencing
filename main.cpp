a8aeb8839f6522d042af83208c256db8111bcb01
Sanpak Ng <>
2021-04-16 01:45:57 +0000

    fixed a typo <string>

--- main.cpp
+++ main.cpp
@@ -1,9 +1,13 @@
 #include <iostream>
-#include <stirng>
+#include <string>
 #include <vector>
 
 using namespace std;
 
 int main() {
-  
+  int score {100};
+  int *score_ptr {&score};
+
+  cout << *score_ptr << endl;
+
 }
